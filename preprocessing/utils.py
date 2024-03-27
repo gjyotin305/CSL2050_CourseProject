4c69249a7cde471b05936e250b215488a290a8c5
jyotinc360 <b22ai063@iitj.ac.in>
2024-03-27 16:20:58 +0530

    PEP8 formatting

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,23 +1,29 @@
 from sklearn.decomposition import PCA
 import numpy as np
 
+
 def pca_for_rgb(img, n_components):
-  red = img[:, :, 0]
-  green = img[:, :, 1]
-  blue = img[:, :, 2]
+    red = img[:, :, 0]
+    green = img[:, :, 1]
+    blue = img[:, :, 2]
 
-  try:  
-    pca = PCA(n_components=n_components)
+    try:
+        pca = PCA(n_components=n_components)
 
-    red_transformed = pca.fit_transform(red)
-    red_inverted = pca.inverse_transform(red_transformed)
+        red_transformed = pca.fit_transform(red)
+        red_inverted = pca.inverse_transform(red_transformed)
 
-    green_transformed = pca.fit_transform(green)
-    green_inverted = pca.inverse_transform(green_transformed)
-    
-    blue_transformed = pca.fit_transform(blue)
-    blue_inverted = pca.inverse_transform(blue_transformed)
-    img_compressed = (np.dstack((red_inverted, green_inverted, blue_inverted))).astype(np.uint8)
-    return img_compressed 
-  except:
-    print("Kindly put valid number of components")
+        green_transformed = pca.fit_transform(green)
+        green_inverted = pca.inverse_transform(green_transformed)
+
+        blue_transformed = pca.fit_transform(blue)
+        blue_inverted = pca.inverse_transform(blue_transformed)
+        img_compressed = (
+            np.dstack(
+                (red_inverted,
+                 green_inverted,
+                 blue_inverted))).astype(
+            np.uint8)
+        return img_compressed
+    except BaseException:
+        print("Kindly put valid number of components")
