36163578a916362062e062ee24a65497444e1247
akshatb22me007 <b22cs096@iitj.ac.in>
2024-04-20 02:14:17 +0530

    Model Integrated with Gradio

--- ui_gradio.py
+++ ui_gradio.py
@@ -5,8 +5,8 @@ import os
 from Experiments.Resnet50_classification import retrieve as model1_retrieve
 
 # Define your image retrieval functions for different models
-def model_1(image,num_images):
-    retrived_images = model1_retrieve(image,k=num_images)
+def model_1(image,num_images=3):
+    retrived_images = model1_retrieve(image,k=3)
     return retrived_images
 
 def model_2(image):
@@ -17,11 +17,11 @@ def model_3(image):
     retrived_images =None
     return retrived_images
 
-num_images = gr.Slider(minimum=1, maximum=10, label="Number of Retrieved Images")
-
-model_1_page = gr.Interface(fn=model_1,inputs=[gr.Image(label="Query Image"),num_images],outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(3)])
-model_2_page = gr.Interface(fn=model_2,inputs=gr.Image(label="Query Image"),outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(3)])
-model_3_page = gr.Interface(fn=model_3,inputs=gr.Image(label="Query Image"),outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(3)])
+# num_images = gr.Slider(minimum=1, maximum=10, label="Number of Retrieved Images")
+num_images = 3
+model_1_page = gr.Interface(fn=model_1,inputs=gr.Image(label="Query Image"),outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(num_images)])
+model_2_page = gr.Interface(fn=model_2,inputs=gr.Image(label="Query Image"),outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(num_images)])
+model_3_page = gr.Interface(fn=model_3,inputs=gr.Image(label="Query Image"),outputs=[gr.Image(type="pil", label="Retrieved Images") for _ in range(num_images)])
 
 demo = gr.TabbedInterface([model_1_page, model_2_page,model_3_page], ["Model 1", "Model 2","Model 3"],title = "Image Retrieval System")
 
